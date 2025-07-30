759369f0b6f17780707fdeef879b183c8bf0b66a
Ben Merlotti <benmerlotti@gmail.com>
2025-07-30 10:53:20 -0400

    update sitemap with routes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sitemap } from 'vite-plugin-sitemap'; // <-- Import the plugin
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(),
+    sitemap({ hostname: 'https://motobandit.net',
+      dynamicRoutes: [
+        '/',          // Your homepage
+        '/shop',      // Your shop page
+        '/music',     // Your music page
+        '/videos',    // Your videos page
+        '/unlock',    // Your unlock song page
+        '/contact',   // Your contact page
+      ],
+    })
+  ],
 })
